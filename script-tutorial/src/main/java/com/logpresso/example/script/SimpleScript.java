27f54fdefff00e394f7bdf3561ec9ba67e58f8d2
everclear-- <faditia@gmail.com>
2014-06-24 18:42:49 +0900

    implemented script tutorial

---
+++ script-tutorial/src/main/java/com/logpresso/example/script/SimpleScript.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2014 Eediom Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.logpresso.example.script;
+
+import org.araqne.api.Script;
+import org.araqne.api.ScriptContext;
+import org.araqne.api.ScriptUsage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleScript implements Script {
+	private final Logger logger = LoggerFactory.getLogger(SimpleScript.class);
+	private ScriptContext context;
+	private final SimpleService service;
+
+	public SimpleScript(SimpleService service) {
+		this.service = service;
+	}
+
+	@Override
+	public void setScriptContext(ScriptContext context) {
+		this.context = context;
+	}
+	
+	public void getMessage(String[] args) {
+		context.println("Simple Service Message : " + service.getMessage());
+	}
+	
+	@ScriptUsage(description = "set simple service message")
+	public void setMessage(String[] args) throws InterruptedException {
+		context.print("Simple Service Message? ");
+		String line = context.readLine().trim();
+		service.setMessage(line);
+		logger.info("message set : " + line);
+	}
+
+}
---
+++ script-tutorial/src/main/java/com/logpresso/example/script/SimpleServiceImpl.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2014 Eediom Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.logpresso.example.script;
+
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Invalidate;
+import org.apache.felix.ipojo.annotations.Provides;
+import org.apache.felix.ipojo.annotations.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(name = "simplescript-service-impl")
+@Provides
+public class SimpleServiceImpl implements SimpleService {
+	private int writeInterval = 20000;
+	private String message = "dummy";
+	private MessageWriter writer;
+	private Thread writeMessageThread;
+
+	@Validate
+	@Override
+	public void start() {
+		writer = new MessageWriter(writeInterval, message);
+		writeMessageThread = new Thread(writer, "Simple Message Writer");
+		writeMessageThread.start();
+	}
+
+	@Invalidate
+	@Override
+	public void stop() {
+		writer.doStop = true;
+		synchronized (writer) {
+			writer.notifyAll();
+		}
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public void setMessage(String message) {
+		this.message = message;
+		stop();
+		start();
+	}
+	
+	private class MessageWriter implements Runnable {
+		private final Logger logger = LoggerFactory.getLogger(MessageWriter.class.getName());
+		private volatile int writeInterval;
+		private final String message;
+		
+		private volatile boolean doStop = false;
+		
+		public MessageWriter(int writeInterval, String message) {
+			this.writeInterval = writeInterval;
+			this.message = message;
+		}
+
+		@Override
+		public void run() {
+			while (true) {
+				if (doStop)
+					break;
+				
+				logger.info(message);
+				try {
+					Thread.sleep(writeInterval);
+				} catch (InterruptedException e) {
+					break;
+				}
+			}
+		}
+		
+	}
+
+}
