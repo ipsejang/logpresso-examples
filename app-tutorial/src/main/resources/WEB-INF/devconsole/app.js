797f542a6d843372c5845e666ac71f654a1e8787
gotoweb <igotoweb@gmail.com>
2014-10-21 17:59:08 +0900

    logdb groc comment updated

--- app-tutorial/src/main/resources/WEB-INF/devconsole/app.js
+++ app-tutorial/src/main/resources/WEB-INF/devconsole/app.js
@@ -4,6 +4,9 @@
 
 // 로그프레소 메시지버스란, 로그프레소 코어가 웹콘솔 등과 통신하는 채널을 말하는 것으로 WebSocket 기술을 사용하며, UI에서는 로그프레소에 내장되어 있는 WebSocket 서버와 통신할 수 있도록 만들어져 있습니다.
 
+// 메시지버스 명령은 `socket` 서비스의 `send` 메소드를 통해 실행할 수 있으며,
+// 이 앱에서는 로그프레소 CLI의 `msgbus.plugin` 명령을 통해 확인할 수 있는 다양한 메시지버스 명령을, UI에 준비된 입력 상자를 통해 실행할 수 있게끔 만들어져 있습니다.
+
 // ### 앱의 시작
 // 앱은 괄호로 닫혀진 [즉시 실행하는 anonymous function](http://markdalgleish.com/2011/03/self-executing-anonymous-functions/) 의 형태로 만들어져야 합니다.
 (function() {
--- app-tutorial/src/main/resources/WEB-INF/logdb/app.js
+++ app-tutorial/src/main/resources/WEB-INF/logdb/app.js
@@ -1,108 +1,151 @@
+// # 쿼리 결과 페이징 예제
+
+// 이 로그프레소 앱은 쿼리 및 결과에 대한 페이징 예제를 보여주는 앱입니다.
+
+// 로그프레소는 쿼리 엔진을 통하여 로그스토리지에 적재된 로그들을 쿼리할 수 있습니다. 이 예제는 직접 쿼리문을 입력하여 조회하고, 그 결과를 일정 단위로 끊어서 페이징 할 수 있도록 제작되었습니다.
+// 쿼리는 다음과 같은 과정으로 이루어집니다.
+
+// <그림> create, started, push 등등의 과정 설명
+
+// 로그프레소가 제공하는 API를 이용하여 각각의 과정, 즉 이벤트 발생에 따른 이벤트 핸들러를 등록할 수 있습니다.
+
+// 이 과정에 대한 자세한 설명은 다음을 참조하십시오.
+
+// 이 때 중요한 것은 반드시 쿼리 결과의 사용이 끝나면 삭제해줘야 한다는 것입니다. 그렇지 않다면 더이상 사용하지 않는 쿼리 결과가 메모리에 계속 남아있게 되어 성능에 지장을 줄 수 있습니다.
+
+// 이 소스를 통해 쿼리 및 쿼리 결과를 조회하는 방법에 대해 알수 있습니다. 또한 쿼리 및 쿼리 결과 조회에 도움을 주는 UI 컨트롤들을 소개하고, 이를 이용하는 법을 알아봅니다.
+
+// ### UI 컨트롤 소개
+// index.html 파일을 보면 `<query-input>`, `<table-view-with-pager>` 등의 directive 를 볼 수 있습니다.
+
+// 이는 로그프레소가 제공하는 UI 컨트롤의 일부로, 쿼리문을 입력해서 쿼리하고, 그 결과를 테이블 형태로 페이징해서 보여줄 수 있는 기능들을 제공하고 있습니다.
+
+// #### &lt;query-input&gt; 컨트롤
+
+// `<query-input>`컨트롤은 쿼리 입력창과 검색(쿼리) 버튼, 그리고 쿼리 도중 쿼리를 중단할 수 있는 버튼을 제공하고 있습니다.
+
+// ```html
+// <query-input
+// 	id="demo-qi-1"
+// 	ng-model="queryResult"
+// 	ng-query-string="queryString"
+// 	ng-page-size="numPageSize"
+// 	ng-pid="numPid"
+// 	on-head="onHead($helper)"
+// 	on-failed="onFailed($raw, $type, $note)">
+// </query-input>
+// ```
+
+// 위 속성 중 반드시 필요한 것은 `ng-model`, `ng-query-string`, `ng-page-size`, `ng-pid`로, 이에 대한 설명은 다음과 같습니다.
+// * `ng-model` 쿼리 결과를 적재할 모델입니다.
+// * `ng-query-string` 쿼리문을 바인딩할 모델입니다.
+// * `ng-page-size` 수백, 수천만개의 쿼리 결과들을 일정 단위로 끊어서 받아올 수 있도록 페이징 크기를 설정하는 모델입니다.
+// * `ng-pid` 앱의 고유 아이디입니다.
+
+// 기타 속성 및 메소드들에 대한 자세한 설명은 API 문서를 참조하십시오.
+
+// #### &lt;table-view-with-pager&gt; 컨트롤
+
+// `<table-view-with-pager>`컨트롤은 단순히 테이블과 페이저를 합쳐놓은 컨트롤로, 배열로 된 결과물을 페이징해서 보여줄 수 있도록 제작된 컨트롤입니다. 여기에 이벤트 핸들러를 등록해 페이지가 변경될 때마다 표시될 모델을 변경하는 과정을 코딩합니다.
+
+// ```html
+// <table-view-with-pager
+// 	ng-model="queryResult"
+// 	ng-total-count="numTotalCount"
+// 	ng-items-per-page="numPageSize"
+// 	ng-page-size="numPagerPageSize"
+// 	on-page-change="changePage($idx)">
+// </table-view-with-pager>
+// ```
+
+// * `ng-model` 테이블에 표시할 (페이징된) 모델입니다.
+// * `ng-total-count` 페이징해서 보여줄 전체 모델의 총 row의 갯수를 바인딩합니다.
+// * `ng-items-per-page` 한 페이지에 몇개의 row를 표시할 지를 결정합니다.
+// * `ng-page-size` 페이저 자체에 몇개의 인덱스를 표시할 지를 결정합니다.
+// * `on-page-change` 페이지가 변경될 때마다 실행할 이벤트 핸들러입니다. `$idx`로 인덱스를 넘겨줄 수 있습니다.
+
+// 마찬가지로 기타 속성 및 메소드들에 대한 자세한 설명은 API 문서를 참조하십시오.
+
+// 이제 자바스크립트 코드를 통해 이러한 컨트롤을 어떻게 사용하는지 알아봅니다.
+
+// ### 앱의 시작
 (function() {
+	// ### 고유 아이디 부여
+	// 앱이 가지는 고유 아이디를 받아옵니다.
+	var pid = LOGPRESSO.process.getPid();
 
-	function SampleLogdbServiceController($scope, socket, serviceLogdb) {
-
-		console.log('-------------- SampleLogdbServiceController -----------------')
-		var instance = serviceLogdb.create(12341234);
-
-		console.clear();
-		console.log(instance);
-
-		var q = instance.query('table sys_query_logs', 100)
-
-		console.log(q);
-
-		q.created(function(m) {
-			console.log('created', m);
-		})
-		.started(function(m) {
-			console.log('started', m);
-		})
-		.onStatusChange(function(m) {
-			console.log('onStatusChange', m);
-		})
-		.onHead(function(helper) {
-			console.log('onHead', helper);
-		})
-		.onTail(function(helper) {
-			console.log('onTail', helper);
-
-			helper.getResult(function(m) {
-				console.log('getResult', m);
-			});
-
-
-		})
-		.loaded(function(m) {
-			console.log('loaded', m);
-		})
-		.failed(function(m, raw) {
-			console.log('failed', m, raw);
-		});
-
-
-	}
-
+	// ### 메인 컨트롤러 작성
 	function SampleLogdbDirectiveController($scope, socket, serviceLogdb) {
+
+		// ### 모델 정의
+
+		// `<query-input>`에서 쓰이는 모델들입니다.
+		$scope.numPid = pid;
 		$scope.queryString = '';
-		$scope.queryResult;
+		$scope.queryResult = [];
+
+		// `<query-input>`에서의 페이징 크기는 `<table-view-with-pager>`에서 한 페이지에 보여질 row 갯수와 같아야(혹은 커야) 한 번의 쿼리에 한 페이지를 가득 채울 수 있습니다.
 		$scope.numPageSize = 20;
-		
+
+		// `<table-view-with-pager>`에서 쓰이는 모델들입니다.
 		$scope.numTotalCount = 0;
-		$scope.numPageSize = 50;
 		$scope.numPagerPageSize = 10;
+
+		// 현재 페이지입니다.
 		$scope.numCurrentPage = 0;
 
-		$scope.changePage = function (idx) {
-			$scope.numCurrentPage = idx;
-			angular.element('#demo-qi-1')[0].getInstance().getResult(idx * $scope.numPageSize, $scope.numPageSize, function(m) {
-				$scope.$apply(function() {
-					$scope.modelTable = m.body.result;
-				});
-			});
-		}
+		// ### 쿼리 이벤트에 따른 결과 출력
+		// `on-head` 이벤트 핸들러는, 쿼리 후 검색된 결과가 페이지 사이즈에 도달했을 때, 이 예제에서는 처음 20개의 결과를 조회할 수 있을 때 발생하는 이벤트입니다.
 
-		$scope.numPid = 123;
-
+		// 그러나 즉시 결과를 받아올 수 있는 것은 아니며, `$helper`를 통해 넘겨진 헬퍼에 담긴 `getResult` 메소드를 통해 결과를 받아올 수 있습니다.
 		$scope.onHead = function(helper) {
-			console.log('onHead', helper)
+			// `getResult(callback)` 메소드는 callback 을 파라미터로 넘기며, 이 callback 함수의 첫번째 인자에 결과가 담겨져서 넘어옵니다.
 			helper.getResult(function(m) {
-				console.log(m);
 
-				if($scope.optionResultCursor === 'head') {
-					$scope.modelTable = m.body.result;
-					$scope.$apply();
-				}
+				// 결과를 받아서 모델에 assign 합니다.
+				$scope.queryResult = m.body.result;
+				$scope.$apply();
 			});
 		}
 
-		$scope.onTail = function(helper) {
-			console.log('onTail', helper)
-			$scope.numTotalCount = helper.message.body.total_count;
-			// helper.getResult(function(m) {
-			// 	console.log(m);
+		// ### 페이지 변경 이벤트
+		// `on-page-change`이벤트에 `$idx`라고 인덱스를 넘겨주면, 변경된 인덱스(쪽수)를 알아낼 수 있습니다.
+		$scope.changePage = function (idx) {
+			// 현재 페이지를 할당합니다.
+			$scope.numCurrentPage = idx;
 
-			// 	if($scope.optionResultCursor === 'tail') {
-			// 		$scope.modelTable = m.body.result;
-			// 		$scope.$apply();
-			// 	}
-			// });
-$scope.$apply();
+			// 쿼리 인스턴스를 반환하고, 인스턴스에 내장된 `getResult` 함수를 이용해 원하는 구간의 쿼리 결과를 바인딩 합니다.
+
+			// 이 과정에서는 두가지 주목할 점이 있는데, 첫번째는 UI 컨트롤로부터 인스턴스를 얻어오는 과정이며, 두번째로는 `getResult(offset, limit, callback)`함수를 사용하는 과정입니다. 다음은 이러한 과정에 대한 자세한 설명입니다.
+
+			// * 먼저 css 셀렉터를 이용해서 `<query-input>` 컨트롤을 선택합니다.
+			var element = angular.element('#demo-qi-1')[0];
+
+			// * `<query-input>`에는 `getInstance`라는 메소드가 있습니다. 이를 통해 쿼리 인스턴스를 가져올 수 있습니다.
+			var instance = element.getInstance();
+
+			// * 쿼리 인스턴스의 `getResult(offset, limit, callback)` 함수를 이용합니다.
+			// 얻어오고자 하는 값은 [쪽수] * [한페이지에 보여질 row 갯수] 로부터 [row 갯수]만큼입니다. 순서대로 파라미터로 넘겨줍니다.
+			instance.getResult(idx * $scope.numPageSize, $scope.numPageSize, function(m) {
+				$scope.modelTable = m.body.result;
+				$scope.$apply()
+			});
+
+			// > **주의!**
+			// > 이는 `onHead`, `onTail` 이벤트의 helper 에 딸린 `getResult(callback)` 메소드와는 넘기는 파라미터가 다릅니다.
+			// > helper의 `getResult(callback)`는 컨트롤에 지정해준 대로 limit이 자동으로 지정되지만, 인스턴스의 `getResult(offset, limit, callback)`에는 offset, limit을 지정해야 합니다.
 		}
 
-		$scope.optionResultCursor = 'head';
-
-		$scope.modelTable = [];
-
+		// ### 에러 처리
+		// 잘못된 쿼리문을 입력할 경우 등에 대하여 에러를 표시할 필요가 있습니다.
+		// 이는 `<query-input>`의 `on-failed` 이벤트를 이용해서 처리할 수 있습니다.
+		// `$raw`에 에러에 대한 전체 정보가 담겨져 있습니다.
+		$scope.onFailed = function(raw, type, note) {
+			alert(raw[0].errorCode);
+		}
 	}
 
-	function SampleLogdbController($scope, socket) {
-		$scope.hello = 'world';
-
-	}
-
-	extension.global.addController(SampleLogdbServiceController);
+	// ### 메인 컨트롤러 등록
+	// 컨트롤러를 등록해서 로그프레소가 이 앱의 컨트롤러를 인식할 수 있도록 합니다.
 	extension.global.addController(SampleLogdbDirectiveController);
-	extension.global.addController(SampleLogdbController);
-	
 })();
